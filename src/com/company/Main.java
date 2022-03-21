d421f3a3a61ec29707ed3e584afecd5659f99172
spektor227 <spektor227@rambler.ru>
2022-03-21 17:40:51 +0300

    division by zero

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,17 @@ public class Main {
     public static void main(String[] args) {
 
         Calculator calc = Calculator.instance.get();
+        try {
+            int a = calc.plus.apply(7, 2);
+            int b = calc.minus.apply(4, 4);
+            int c = calc.devide.apply(a, b);
+            int d = calc.pow.apply(a);
+            calc.println.accept(c);
+            calc.println.accept(d);
+        } catch (ArithmeticException exception) {
+            System.out.println("Деление на ноль.( значение b = 0 - деление на возможно.)");
+        }
 
-        int a = calc.plus.apply(7, 2);
-        int b = calc.minus.apply(4, 1);
-        int c = calc.devide.apply(a, b);
-        int d = calc.pow.apply(a);
-
-        calc.println.accept(c);
-        calc.println.accept(d);
 
     }
 }
